cf72f4675e7f4df9131d10c108f102722a32efa2
corbin <c_luck@mail.fhsu.edu>
2026-03-25 11:59:42 -0500

    Added a back button to the edit page
    Also added the admin username the ability to edit any post

--- flaskr/ticket.py
+++ flaskr/ticket.py
@@ -60,7 +60,7 @@ def get_post(id, check_author=True):
     if post is None:
         abort(404, f"Post id {id} doesn't exist.")
 
-    if check_author and post['author_id'] != g.user['id']:
+    if check_author and post['author_id'] != g.user['id'] and g.user['username'] != 'admin':
         abort(403)
 
     return post
